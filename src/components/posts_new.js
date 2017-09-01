f3d85ee1b359287a40bd5cff56dfbd366043b996
Eleanor Mark <eleanormark@gmail.com>
2017-08-31 19:58:51 -0700

    added home route to cancel button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
@@ -42,6 +43,7 @@ class PostsNew extends Component {
         <Field label="Categories" name="nameCategories" component={this.renderField} />
         <Field label="Content" name="nameContent" component={this.renderField} />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
